bab569e2b3c2259bf10b8fff8161382922041c5c
Mats Schjølberg <mats@schjolberg.net>
2016-08-25 23:11:47 +0200

    Fixed deplot script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,9 +7,9 @@ set -e
 if [ ! -z "$TRAVIS" ]; then
   echo "Decrypting ssh-key and adding"
   openssl aes-256-cbc -K $encrypted_b3a9f4cfdc6e_key -iv $encrypted_b3a9f4cfdc6e_iv -in travis.enc -out travis -d
-  chmod 600 travis-key
+  chmod 600 travis
   eval "$(ssh-agent)"
-  ssh-add travis-key
+  ssh-add travis
 fi
 
 echo "Running remote SSH-script"
