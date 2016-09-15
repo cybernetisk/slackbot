55b5103b408d682370406b1bac11c2cb63a8cc37
Mats Schjølberg <mats@schjolberg.net>
2016-09-15 16:31:44 +0200

    Changed dagens list word to list instead of help

--- plugins/dagens.py
+++ plugins/dagens.py
@@ -13,7 +13,7 @@ def dagens(message, cafeteria=None):
     sio = Dagens()
     if cafeteria is None:
         message.reply(sio.get_dinner_from_sio())
-    elif cafeteria == 'help':
+    elif cafeteria == 'list':
         message.reply(sio.help())
         return
     else:
