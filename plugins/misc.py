344462a2617c268107ab70c63aa175423fdbd6b5
Mats Schjølberg <mats@schjolberg.net>
2016-09-15 16:28:43 +0200

    Some nicer printing.

--- plugins/misc.py
+++ plugins/misc.py
@@ -1,11 +1,9 @@
 from slackbot.bot import respond_to, listen_to
 import datetime
-import calendar
 from pytz import timezone
 
 @respond_to(r'dag$')
 def day(message, func=None):
     oslo = timezone('Europe/Oslo')
     now = datetime.datetime.now(tz=oslo)
-    message.reply(calendar.day_name[now.weekday()])
-
+    message.reply(now.strftime('Date is %A %Y/%m/%d'))
