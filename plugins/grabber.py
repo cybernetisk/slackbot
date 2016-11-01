d64c906d5004e4a3dd27b97b6c5c4972c3c4dc92
Adrian Helle <adriah@ifi.uio.no>
2016-11-01 18:28:13 +0100

    fixed the regex

--- plugins/grabber.py
+++ plugins/grabber.py
@@ -4,8 +4,8 @@ from slackbot.bot import respond_to, listen_to
 
 from BeautifulSoup import BeautifulSoup
 
-@listen_to(r'^.*http.')
-@listen_to(r'^.*https.')
+@listen_to(r'^http.*')
+@listen_to(r'^https.*')
 def grabber(message, url):
     soup = BeautifulSoup(urllib2.urlopen(url))
     message.reply("Title:")
