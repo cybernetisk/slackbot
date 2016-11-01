d633b1f5e995df329f704dda47f0d54a9b0a77b5
Mats Astrup Schjølberg <mats@schjolberg.net>
2016-11-01 23:18:54 +0100

    Added some nicer error messages

--- plugins/in.py
+++ plugins/in.py
@@ -16,11 +16,18 @@ def wallet_to_string(wallet):
 @respond_to(r'^vouchers$')
 @respond_to(r'^vouchers (.*)')
 def vouchers(message, username=None):
+    printed = False
     if username is None:
         if 'user' in message.body:
             user = get_user_from_message(message)
             username = user['name']
     wallets = get_from_api(base_url + 'voucher/wallets?user=' + username)
+    if len(wallets) is 0:
+        message.reply('There isn\'t any information for %s' % username)
     for wallet in wallets:
         if semester_is_valid(wallet['semester']):
+            printed = True
             message.reply(wallet_to_string(wallet))
+    if printed is False:
+        message.reply('I can\'t find a wallet for %s this semester' % username)
+
