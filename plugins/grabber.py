88e295fb2e1a6bc1654ceb535fe54c1518f08d95
Adrian Helle <adriah@ifi.uio.no>
2016-11-01 18:54:28 +0100

    changed from urllib2 to urllib

--- plugins/grabber.py
+++ plugins/grabber.py
@@ -1,12 +1,12 @@
-import urllib2
-
 from slackbot.bot import respond_to, listen_to
+
+import urllib
 
 from BeautifulSoup import BeautifulSoup
 
 @listen_to(r'^http.*')
 @listen_to(r'^https.*')
 def grabber(message, url=None):
-    soup = BeautifulSoup(urllib2.urlopen(url))
+    soup = BeautifulSoup(requests.get(url))
     message.reply("Title:")
     message.reply(soup.title.string)
