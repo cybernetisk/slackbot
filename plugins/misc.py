b9098e388ad20c2463fe26a810bf86d5960fc878
Mats Astrup Schjølberg <mats@schjolberg.net>
2016-11-19 21:22:23 +0100

    Added function to get a random commit message.

--- plugins/misc.py
+++ plugins/misc.py
@@ -1,5 +1,6 @@
 from slackbot.bot import respond_to, listen_to
 import datetime
+import requests
 from pytz import timezone
 
 @respond_to(r'^dag$')
@@ -11,3 +12,10 @@ def day(message, func=None):
 @respond_to(r'^ping$')
 def ping(message):
     message.reply('Pong!')
+
+@respond_to(r'^commit$')
+@listen_to(r'^!commit$')
+def commit(message):
+    url = "http://whatthecommit.com/index.txt"
+    commitmessage = requests.get(url).text
+    message.reply(commitmessage)
