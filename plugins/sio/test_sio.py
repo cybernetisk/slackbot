d69293eaa9f4f76e31838188ed5c0400728b93bd
Maximiliano Horta <maximilianohorta94@gmail.com>
2018-10-24 22:10:10 +0200

    added ifi to list and some tests

--- plugins/sio/test_sio.py
+++ plugins/sio/test_sio.py
@@ -1,6 +1,13 @@
 from plugins.sio.sio import Dagens
 
-
 def test_dagen_config():
     d = Dagens()
     assert d.cafeterias['ifi'] == 284
+
+def test_dagens_config2():
+    d = Dagens()
+    assert d.get_dinner_cafeteria('ifi') != 'Cafeteria not found'
+
+def test_dagens_dagens():
+    d = Dagens()
+    assert d.get_dinner_cafeteria('ifi').find("dagens" and "pris")
