3db347b9cda36ee2e4d9a2fde84284d36cc2e07f
audunjoh <42673279+audunjoh@users.noreply.github.com>
2019-02-05 15:13:11 +0100

    Added case and whitespace insensitivity
    For when you're hungry and yell "!DAGENS" or do "!Dagens     DaNa".

--- plugins/sio/dagens.py
+++ plugins/sio/dagens.py
@@ -5,18 +5,18 @@ from slackbot.bot import respond_to, listen_to
 from plugins.sio.sio import Dagens
 
 
-@respond_to(r'^dagens$')
-@respond_to(r'^dagens (.*)')
-@listen_to(r'^!dagens$')
-@listen_to(r'^!dagens (.*)')
+@respond_to(r'(?i)^dagens\s*$')
+@respond_to(r'(?i)^dagens\s\s*(.*)')
+@listen_to(r'(?i)^!dagens\s*$')
+@listen_to(r'(?i)^!dagens\s\s*(.*)')
 def dagens(message, cafeteria=None):
     sio = Dagens()
     if not cafeteria:
         message.reply(sio.get_dinner_from_sio())
-    elif cafeteria == 'dana':
+    elif cafeteria.lower() == 'dana':
         message.reply("Børek. Varme 1337?")
-    elif cafeteria == 'list':
+    elif cafeteria.lower() == 'list':
         message.reply(sio.help())
         return
     else:
-        message.reply(sio.get_dinner_from_sio(cafeteria))
+        message.reply(sio.get_dinner_from_sio(cafeteria.lower()))
